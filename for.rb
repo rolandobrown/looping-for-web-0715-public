27423c311191a735a1361b619dd3fa2fdea684c0
rolandobrown <rolando.brown@gmail.com>
2015-08-02 22:16:25 -0400

    all done

--- for.rb
+++ for.rb
@@ -1,7 +1,18 @@
 
 def using_for
 	checklist = 1..10
-	#your code here
+	for item in checklist
+    puts "Wingardium Leviosa" 
+  end
 end
 
+## DONE
 
+# Fork and clone this lab
+# puts "Wingardium Leviosa" for each item in the checklist
+# puts "Wingardium Leviosa" ten (10) times
+# Fill out the content of the using_for method in the for.rb 
+
+## PENDING
+
+# all done.
